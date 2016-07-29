81fb57f0236f19be201d54d4a16ac62a81e3184c
Scibby <scibandrew@gmail.com>
2016-07-29 23:20:13 +0200

    Added the ability to customise the game title.

--- src/scibby/core/GameMain.java
+++ src/scibby/core/GameMain.java
@@ -2,8 +2,8 @@ package scibby.core;
 
 public abstract class GameMain{
 
-	protected GameContainer init(int width, int height, GameContainer gc){
-		gc = new GameContainer(width, height, 60, "Title");
+	protected GameContainer init(int width, int height, GameContainer gc, String title){
+		gc = new GameContainer(width, height, 60, title);
 		gc.start();
 		return gc;
 	}
