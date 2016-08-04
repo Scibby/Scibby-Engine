8f282de3a3809ea5fe58564f0b82e2a288a41ff3
Scibby <scibandrew@gmail.com>
2016-08-04 19:42:33 +0200

    Added a null check to the getTile method.

--- src/scibby/level/Level.java
+++ src/scibby/level/Level.java
@@ -11,19 +11,26 @@ import scibby.entities.Tile;
 public abstract class Level{
 
 	private static int currentLevel;
-	
+
 	private static Level level;
-	
+
+	protected final int WIDTH, HEIGHT;
+
 	public static ArrayList<Level> levels = new ArrayList<Level>();
-	
+
 	protected LinkedList<Entity> entities = new LinkedList<Entity>();
-	
+
 	protected HashMap<Integer, Tile> tiles = new HashMap<Integer, Tile>();
-	
+
+	public Level(int width, int height){
+		this.WIDTH = width;
+		this.HEIGHT = height;
+	}
+
 	public static void tick(){
 		if(currentLevel == 0) return;
 		for(Entity e : level.entities){
-			e.tick();;
+			e.tick();
 		}
 	}
 
@@ -33,24 +40,29 @@ public abstract class Level{
 			e.render(g);
 		}
 	}
-	
-	public static Tile getTile(int x, int y, int width){
-		return level.tiles.get(x + y * width);
+
+	public static Tile getTile(int x, int y){
+		Tile tile = level.tiles.get(x + y * level.WIDTH);
+		if(tile != null) return tile;
+		return tile;
 	}
-	
+
 	public static void add(Entity entity){
 		if(currentLevel == 0) return;
 		level.entities.add(entity);
 	}
-	
+
 	public static void remove(Entity entity){
 		if(currentLevel == 0) return;
-		level.entities.remove(entity); 
+		level.entities.remove(entity);
 	}
-	
+
 	public static void setLevel(int levelNumber){
 		currentLevel = levelNumber;
 		level = levels.get(levelNumber - 1);
 	}
-	
+
+	public abstract int getHEIGHT();
+
+	public abstract int getWIDTH();
 }
