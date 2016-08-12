0de433c4d988733ea27dfb6dbb9794b3b6ab6014
Scibby <scibandrew@gmail.com>
2016-08-12 19:43:32 +0200

    Changed the x and y variables to doubles.

--- src/scibby/entities/Entity.java
+++ src/scibby/entities/Entity.java
@@ -5,11 +5,11 @@ import java.awt.Rectangle;
 
 public abstract class Entity{
 
-	public int x, y;
+	public double x, y;
 
 	public int width, height;
 
-	public Entity(int x, int y, int width, int height){
+	public Entity(double x, double y, int width, int height){
 		this.x = x;
 		this.y = y;
 		this.width = width;
@@ -19,25 +19,5 @@ public abstract class Entity{
 	public abstract void tick();
 
 	public abstract void render(Graphics2D g);
-
-	public Rectangle getBounds(){
-		return new Rectangle(x, y, width, height);
-	}
-
-	public Rectangle getBoundsTop(){
-		return new Rectangle(x + 6, y, width - 12, 12);
-	}
-
-	public Rectangle getBoundsBottom(){
-		return new Rectangle(x + 6, y + (height - 12), width - 12, 12);
-	}
-
-	public Rectangle getBoundsLeft(){
-		return new Rectangle(x, y + 6, 6, height - 12);
-	}
-
-	public Rectangle getBoundsRight(){
-		return new Rectangle(x + (width - 6), y + 6, 6, height - 12);
-	}
 
 }
