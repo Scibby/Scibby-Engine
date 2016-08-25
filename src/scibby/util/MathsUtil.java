188f8ea94b9b772dcd97c1599fc63a459de96022
Scibby <scibandrew@gmail.com>
2016-08-25 23:00:05 +0200

    Rearraged the variables in the clamp method.

--- src/scibby/util/MathsUtil.java
+++ src/scibby/util/MathsUtil.java
@@ -4,7 +4,7 @@ public class MathsUtil{
 
 	private MathsUtil(){}
 	
-	public static int clamp(int min, int max, int value){
+	public static int clamp(int value, int min, int max){
 		if(value < min) return min;
 		if(value > max) return max;
 		return value;
